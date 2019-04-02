bb0cbd2098b7b9b8cc5f7761a051136c53f18e05
miguelyurivilca <miguel_y_r@hotmail.com>
2019-04-01 21:28:04 -0500

    reading input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <string>
+#include <list>
+#include <algorithm>
 
-using namespace std;
+int main(int charc, char **argv)
+{
+    std::string word;
+    std::list<std::string> words;
+    std::list<std::string> lower_case_words;
 
-int main() {
-	return EXIT_SUCCESS;
+    while (std::cin >> word)
+    {
+        if (word == "#") break;
+        words.push_front(word);
+        std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+        lower_case_words.push_front(word);
+    }
+
+    for (auto i : lower_case_words)
+        std::cout << i << '\n';
+	return (0);
 }
 

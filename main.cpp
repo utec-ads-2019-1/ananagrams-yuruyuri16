ae8b4895808d364d27a46884ffe424ec5b261efd
miguelyurivilca <miguel_y_r@hotmail.com>
2019-04-02 12:46:19 -0500

    character separation of the string

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@
 #include <list>
 #include <algorithm>
 
+char *get_configs(std::string word)
+{
+    int size;
+    int i;
+    char *chars;
+
+    size = word.size();
+    chars = new char[size];
+    for (i = 0; i < size; ++i)
+    {
+        chars[i] = word[i];
+    }
+    return chars;
+}
+
 int main(int charc, char **argv)
 {
     std::string word;
@@ -16,9 +31,7 @@ int main(int charc, char **argv)
         std::transform(word.begin(), word.end(), word.begin(), ::tolower);
         lower_case_words.push_front(word);
     }
-
     for (auto i : lower_case_words)
-        std::cout << i << '\n';
+        std::cout << get_configs(i)[0] << '\n';
 	return (0);
 }
-
